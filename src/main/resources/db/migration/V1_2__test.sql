042f6ffae8b5030d736ffa5a6ddae8cbc248c08c
Patryk Witkowski <218167@student.pwr.edu.pl>
2018-10-14 21:03:32 +0200

    sql update due to displaying timetable changes

--- src/main/resources/db/migration/V1_2__test.sql
+++ src/main/resources/db/migration/V1_2__test.sql
@@ -1,5 +1,5 @@
-insert into timetables(timetable_id, user_id, name, semester, quality, generation_date, timetable_type)
-values (1, 1, 'test_timetable', 1, 1, NOW(), 'VALID');
+insert into timetables(timetable_id, user_id, name, semester, semester_start_date, semester_end_date, quality, generation_date, timetable_type)
+values (1, 1, 'test_timetable', 1, '2018-10-01', '2019-01-30', 1, NOW(), 'INVALID');
 
 insert into enrollment_groups(enrollment_id, user_id, name, ects_sum, semester)
 values (1, 1, 'zapisowa_1', 30, 1);
@@ -7,8 +7,5 @@ values (1, 1, 'zapisowa_1', 30, 1);
 insert into classes(class_id, enrollment_id, name, ects)
 values (1, 1, 'analiza', 12);
 
-insert into courses(course_id, class_id, timetable_id, group_code, courses_date, courses_place, lecturer, even_week, free_places)
-values (1, 1, 1, 'CODE_1', '2018-10-14 08:20:11', 'w dupie', 'debil', 0, 1);
-
-
-
+insert into courses(course_id, class_id, timetable_id, group_code, course_start_time, course_end_time, course_day, courses_place, lecturer, even_week, free_places)
+values (1, 1, 1, 'CODE_1', '08:20:00', '10:00:00', 'MONDAY', 'w dupie', 'debil', 0, 1);
