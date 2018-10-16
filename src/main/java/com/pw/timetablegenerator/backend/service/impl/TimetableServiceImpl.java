04dfffdcb0c500927a02186681665938c65e32f5
Patryk Witkowski <218167@student.pwr.edu.pl>
2018-10-16 22:01:32 +0200

    timetables filtering added

--- src/main/java/com/pw/timetablegenerator/backend/service/impl/TimetableServiceImpl.java
+++ src/main/java/com/pw/timetablegenerator/backend/service/impl/TimetableServiceImpl.java
@@ -4,10 +4,12 @@ import com.pw.timetablegenerator.backend.entity.Timetable;
 import com.pw.timetablegenerator.backend.entity.User;
 import com.pw.timetablegenerator.backend.jpa.TimetableRepository;
 import com.pw.timetablegenerator.backend.service.TimetableService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TimetableServiceImpl implements TimetableService {
@@ -17,7 +19,31 @@ public class TimetableServiceImpl implements TimetableService {
 
     @Override
     public List<Timetable> findTimetables(User user, String value) {
-        return timetableRepository.findByOwner(user);
+        List<Timetable> timetables = timetableRepository.findByOwner(user);
+
+        if(StringUtils.isNotBlank(value)){
+            List<Timetable> filteredTimetables = filterTimetablesByName(timetables, value);
+            filteredTimetables = filterTimetablesByStatus(timetables, filteredTimetables, value);
+            timetables = filteredTimetables;
+        }
+
+        return timetables;
+    }
+
+    private List<Timetable> filterTimetablesByStatus(List<Timetable> timetables,
+                                                     List<Timetable> filteredTimetables, String value) {
+        if(filteredTimetables.isEmpty()){
+            filteredTimetables = timetables.stream()
+                    .filter(timetable -> StringUtils.containsIgnoreCase(timetable.getTimetableType().toString(), value))
+                    .collect(Collectors.toList());
+        }
+        return filteredTimetables;
+    }
+
+    private List<Timetable> filterTimetablesByName(List<Timetable> timetables, String value) {
+        return timetables.stream()
+                .filter(timetable -> StringUtils.containsIgnoreCase(timetable.getName(), value))
+                .collect(Collectors.toList());
     }
 
     @Override
